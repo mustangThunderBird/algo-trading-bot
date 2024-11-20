54445a8266769fa6187918e1567f234d8b7d28f2
Nicholas Facciola <nicholasfacciola@gmail.com>
2024-11-20 08:17:08 -0700

    sentiment scores

--- src/model/qualitative/qual_model.py
+++ src/model/qualitative/qual_model.py
@@ -65,6 +65,89 @@ def fetch_news():
             sn_frames[ticker] = df
     return sn_frames
 
+def basic_cleanup(text):
+    cleaned_text = unicodedata.normalize('NFKD', text).encode('ascii','ignore').decode('utf-8', 'ignore')
+    cleaned_text = re.sub(r'[^a-zA-Z0-9.,;:!?\'\"()\s]', '', cleaned_text)
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
+    # Create a single regular expression pattern that matches any of the filler texts
+    filler_pattern = '|'.join(map(re.escape, filler_texts))
+    # Pattern to match unnecessary whitespace, tabs, and escape characters
+    whitespace_pattern = r'\s+'
+    cleaned_text = re.sub(whitespace_pattern, ' ', re.sub(filler_pattern,'',cleaned_text)).strip() if isinstance(cleaned_text, str) else cleaned_text
+    return cleaned_text
+
+def summarize_article(article_text):
+    summary = summarizer(article_text[:1024], max_length=100, min_length=30, do_sample=False)
+    return summary[0]['summary_text']
+
+def add_sentiment(df:pd.DataFrame):
+    setiments = df['article_text'].apply(lambda text: sentiment_pipeline(text[:1024])[0]['label'] if text != "N/A" else "UNKNOWN")
+    df['sentiment'] = setiments
+    return df
+
+def get_article_text(url):
+    try:     
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+        }
+        response = requests.get(url, headers=headers, timeout=10)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            paragprahs = soup.find_all('p')
+            article_text = ' '.join([p.get_text() for p in paragprahs])
+            article_text = basic_cleanup(article_text)
+            return article_text
+        else:
+            print(f"Failed to get article text from {url} got a response code of {response.status_code}")
+            return "N/A"
+    except Exception as e:
+        print(f"Failed to get article text from {url}: {e}")
+        return "N/A"
+
+def preprocess_data(df:pd.DataFrame) -> pd.DataFrame:
+    try:
+        df = df[['summary', 'link', 'published', 'title']]
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            article_texts = list(executor.map(get_article_text, df['link']))
+        df['article_text'] = article_texts
+        df = add_sentiment(df)
+        return df
+    except Exception as e:
+        return f"Could not process data {e}"
+    
+def preprocess_and_update(ticker, data):
+    df = preprocess_data(data)
+    if isinstance(df, str):
+        print(df)
+    return ticker, df if not isinstance(df, str) else None
+
 if __name__ == "__main__":
     stock_news_frames = fetch_news()
-    print(stock_news_frames)
+    # Run preprocessing in parallel
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = {
+            executor.submit(preprocess_and_update, ticker, data): ticker 
+            for ticker, data in stock_news_frames.items()
+        }
+        
+        for future in as_completed(futures):
+            ticker, result = future.result()
+            stock_news_frames[ticker] = result
+    #print(stock_news_frames.get("INTC"))
+    sentiment_scores = {}
+    for ticker, frame in stock_news_frames.items():
+        if frame is not None:
+            scores = []
+            for sentiment in frame['sentiment']:
+                if sentiment == 'NEGATIVE':
+                    scores.append(-1)
+                elif sentiment == 'POSITIVE':
+                    scores.append(1)
+            if scores != []:
+                sentiment_scores[ticker] = sum(scores) / len(scores)
+            else:
+                sentiment_scores[ticker] = 0
+        else:
+            sentiment_scores[ticker] = 0
+    final_scores = pd.DataFrame.from_dict(sentiment_scores, orient='index', columns=['sentiment_score'])
+    final_scores.to_csv(os.path.join(os.path.dirname(__file__), 'sentiment_scores.csv'), index=True)
